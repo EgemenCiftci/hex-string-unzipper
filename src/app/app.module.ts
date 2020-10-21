f30c779813fd4fea8c5dcb9b7a06ae96373e170b
EgemenCiftci <egemenciftci@gmail.com>
2020-10-21 18:08:19 +0300

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
