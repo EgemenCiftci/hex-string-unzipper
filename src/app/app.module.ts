4f0544d464da1e3f5647c9f9e6b05cd87048ebce
EgemenCiftci <egemenciftci@gmail.com>
2021-06-14 09:54:25 +0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule, Title } from "@angular/platform-browser";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatButtonModule } from "@angular/material/button";
-import { MatInputModule } from "@angular/material/input";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AppComponent } from "./app.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
@@ -15,7 +16,8 @@ import { AppComponent } from "./app.component";
     MatInputModule,
     MatFormFieldModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatCardModule
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
